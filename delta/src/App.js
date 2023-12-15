9674f718fa2bac3ac1f95967ea711a7d9e5796b5
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-12-14 20:57:15 -0500

    added structure

---
+++ delta/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { AgeVerification } from './ageVerification';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Delta</h1>
+
+    </div>
+    <AgeVerification />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ delta/src/ageVerification.js
@@ -0,0 +1,15 @@
+
+
+export function AgeVerification() {
+  return (
+    <div className="Terms-and-Conditions">
+
+    <h1>Terms and Conditions</h1>
+    <p>I am over 18 years of age.</p>
+
+    <input type="checkbox" />
+    </div>
+  );
+}
+
+
